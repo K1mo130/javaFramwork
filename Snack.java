8e9bb866fa40fe3226db04bb77844cc690a57ef5
Karim Altir <mhdkarim48850@gmail.com>
2023-04-24 15:17:56 +0200

    Initial commit

---
+++ Snack.java
@@ -0,0 +1,37 @@
+public class Snack {
+    private int artikelcode;
+    private String naam;
+    private int prijs;
+
+    public Snack(int artikelcode, String naam, int prijs) {
+        this.artikelcode = artikelcode;
+        this.naam = naam;
+        this.prijs = prijs;
+    }
+
+    public int getArtikelcode() {
+        return artikelcode;
+    }
+
+    public void setArtikelcode(int artikelcode) {
+        this.artikelcode = artikelcode;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public int getPrijs() {
+        return prijs;
+    }
+
+    public void setPrijs(int prijs) {
+        this.prijs = prijs;
+    }
+
+
+}
